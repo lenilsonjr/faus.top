a9839241f734555a58988d8b42afa9311f2ecbf0
Lenilson Júnior <me@lenilson.codes>
2017-09-27 22:36:33 -0300

    :art: Faustop icon and change in colors

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,19 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+Rails.application.config.assets.precompile += %w(
+                                                  shortened.js
+                                                )
+
+
+
+Rails.application.config.assets.precompile += %w(
+                                                  global.css
+                                                  shortened.css
+                                                )
+
+Rails.application.config.assets.precompile += %w(
+                                                  favicon.png
+                                                  faustop.jpg
+                                                )
